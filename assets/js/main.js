73ac43759f4fb665cdafa8935bd34994e8983cae
Vaidehi <vkulkarni331@gmail.com>
2025-12-10 14:55:05 +0530

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1021,24 +1021,7 @@
     //     }
     //   }
 
-
-<
-new Swiper('.product-slider', {
-  slidesPerView: 2,
-  spaceBetween: 20,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-  breakpoints: {
-    0: { slidesPerView: 1 },
-    768: { slidesPerView: 2 }
-  }
-});
+    
 
 
 
